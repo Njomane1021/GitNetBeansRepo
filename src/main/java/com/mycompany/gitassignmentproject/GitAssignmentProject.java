71f9955eef0ac687682444511fc7698739a33272
Siphesihle Mapereza <peret@Pereza.com>
2025-04-24 19:40:06 +0200

    committing work

---
+++ src/main/java/com/mycompany/gitassignmentproject/GitAssignmentProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gitassignmentproject;
+
+/**
+ *
+ * @author peret
+ */
+public class GitAssignmentProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
